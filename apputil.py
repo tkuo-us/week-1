b470aba3ec4e300591c8b3939863835b0959ceff
tkuo-us <tkuo@iu.edu>
2025-09-02 03:59:11 +0000

    second commit

--- apputil.py
+++ apputil.py
@@ -1,4 +1,4 @@
-
+# Utility functions for exercises
 def palindrome(string):
     cleaned_string = (
         string.lower()
@@ -9,4 +9,16 @@ def palindrome(string):
         .replace(":", "")
     )
     return cleaned_string == cleaned_string[::-1]
+
+# Check for balanced parentheses
+def parentheses(string):
+    count = 0
+    for char in string:
+        if char == "(":
+            count += 1
+        elif char == ")":
+            count -= 1
+        if count < 0:
+            return False
+    return count == 0
 
