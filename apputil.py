8a0f0dfbbd15053c4a8bdf267b42161e968f21d9
tkuo-us <tkuo@iu.edu>
2025-09-02 03:21:13 +0000

    first commit

--- apputil.py
+++ apputil.py
@@ -1,3 +1,12 @@
 
+def palindrome(string):
+    cleaned_string = (
+        string.lower()
+        .replace(",", "")
+        .replace(".", "")
+        .replace(" ", "")
+        .replace(";", "")
+        .replace(":", "")
+    )
+    return cleaned_string == cleaned_string[::-1]
 
-# add code below ...
